af2196f0f375ad35bf84a1ac223f109102911b8f
KeiJeiYoon <119876100+KeiJeiYoon@users.noreply.github.com>
2023-01-09 16:56:06 +0900

    Update kocom.py

--- kocom.py
+++ kocom.py
@@ -679,7 +679,7 @@ def publish_discovery(dev, sub=''):
 
             'curr_temp_t': 'kocom/room/thermo/{}/state'.format(num),
             'curr_temp_tpl': '{{ value_json.cur_temp }}',
-            'modes': ['off', 'away', 'heat'],
+            'modes': ['off', 'heat'],
             'min_temp': 5,
             'max_temp': 35,
             'ret': 'false',
